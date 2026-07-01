5dd09ba2912901cd173cb9d990428202c748da35
jass-122-dot <samithajasmine2022@email.com>
2026-07-01 16:09:28 +0530

    fix CSS paths in deploy folder

--- doc-chatbot/backend/app.py
+++ doc-chatbot/backend/app.py
@@ -1,22 +1,13 @@
-from flask import Flask, request, Response
-from flask_jwt_extended import JWTManager
+from flask import Flask  # type: ignore[import]
+from flask_jwt_extended import JWTManager  # type: ignore[import]
 from config import Config
 from database import init_db
 from routes.auth import auth_bp
 from routes.docs import docs_bp
-
 app = Flask(__name__)
 app.config.from_object(Config)
-JWTManager(app)
 
-@app.before_request
-def handle_options():
-    if request.method == 'OPTIONS':
-        response = Response()
-        response.headers['Access-Control-Allow-Origin'] = '*'
-        response.headers['Access-Control-Allow-Headers'] = 'Content-Type,Authorization'
-        response.headers['Access-Control-Allow-Methods'] = 'GET,POST,PUT,DELETE,OPTIONS'
-        return response
+JWTManager(app)
 
 @app.after_request
 def add_cors_headers(response):
